57ebc2b8b62ba56ccbef4aeb0b1a6be7ec4fbeb6
Snoodleking <duncaniaria@gmail.com>
2017-05-29 18:48:58 -0400

    initial work on the timer

--- app/components/timer/timer.component.js
+++ app/components/timer/timer.component.js
@@ -1,4 +1,17 @@
+function QuestionController()
+{
+	var controller = this;
+
+	controller.isEnabled = true;
+	controller.timeLimit = 25;
+
+	//controller.
+
+
+}
+
 angular.module( 'trivia' ).component( 'timer', 
 {
 	templateUrl: 'app/components/timer/timer.html',
+	controller: TimerController,
 });
